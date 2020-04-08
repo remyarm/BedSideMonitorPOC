62ff93c6d5ae4bb5c995d55f42502e1cc04f58b2
Remya <retnammalmurukesanr@hcl.com>
2020-04-08 09:11:30 +0530

    Added View for Menu

--- BedSideMonitor/BedSideMonitor/UI/MainView.swift
+++ BedSideMonitor/BedSideMonitor/UI/MainView.swift
@@ -10,23 +10,62 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var mainViewModel: MainViewModel
-    
+    var change = true
     var body: some View {
         NavigationView {
-            ZStack {
-                Spacer()
-                    .layoutPriority(1)
-                GeometryReader { metrics in
-                    if (self.mainViewModel.menuToggle) {
-                        LeftMenuView()
-                            .frame(minWidth: nil, idealWidth: nil, maxWidth: metrics.size.height * 0.30, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .topLeading)
-                            .transition(.move(edge: .leading))
-                        
-                    }
-                }
+            
+            
+            HStack {
                 
+            
+                if (mainViewModel.currentMenu == .none) {
+                HStack {
+                     Spacer()
+                    //                        .layoutPriority(1)
+                                        GeometryReader { metrics in
+                                            if (self.mainViewModel.menuToggle) {
+                                                LeftMenuView()
+                                                    .frame(minWidth: nil, idealWidth: nil, maxWidth: metrics.size.height * 0.30, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .topLeading)
+                                                    .transition(.move(edge: .leading))
+//                                                .background(Color.gray)
+                                            }
+                                        }
+                    }
+                .background(Color.blue)
+                    }
+                else if (mainViewModel.currentMenu == .ECG) {
+                    HStack {
+                    Spacer()
+                    //                        .layoutPriority(1)
+                                        GeometryReader { metrics in
+                                            if (self.mainViewModel.menuToggle) {
+                                                LeftMenuView()
+                                                    .frame(minWidth: nil, idealWidth: nil, maxWidth: metrics.size.height * 0.30, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .topLeading)
+                                                    .transition(.move(edge: .leading))
+//                                                    .background(Color.blue)
+                                                
+                                            }
+                                        }
+                    }
+                    .background(Color.gray)
+                } else if (mainViewModel.currentMenu == .Device1) {
+                    HStack {
+                    Spacer()
+                    //                        .layoutPriority(1)
+                                        GeometryReader { metrics in
+                                            if (self.mainViewModel.menuToggle) {
+                                                LeftMenuView()
+                                                    .frame(minWidth: nil, idealWidth: nil, maxWidth: metrics.size.height * 0.30, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .topLeading)
+                                                    .transition(.move(edge: .leading))
+//                                                    .background(Color.yellow)
+                                                
+                                            }
+                                        }
+                    }
+                    .background(Color.yellow)
+                }
             }
-            .background(Color.gray)
+            
             .edgesIgnoringSafeArea(.all)
             .navigationBarTitle("BedSideMonitor", displayMode: .inline)
             .navigationBarItems(leading:
